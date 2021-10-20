79efc97777981616a48b9dc24b83f100335ae2df
Mahsa Goudarzi <mahsagoodarzi1999@gmail.com>
2021-10-20 22:55:29 +0330

    Cleaned up the react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Hello World!</p>
     </div>
   );
 }
-
-export default App;
